41ceaab1e0ab9f3c6015d67e2567746fc2ff7177
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-05-29 19:13:11 +0700

    Задания исправлены

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,10 +4,10 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d = (Math.pow(b, 2) - 4 * a * c);
-  let x1 = (-b + Math.sqrt(d)) / (2 * a);
-  let x2 = (-b - Math.sqrt(d)) / (2 * a);
 
   if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1, x2); 
   } else if (d === 0) {
     arr.push(-b / (2 * a));
@@ -21,7 +21,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let percentMonthlyRate = percent / 100 / 12;
   let sum = amount - contribution;
   let monthlyPayment = sum * (percentMonthlyRate + (percentMonthlyRate / (Math.pow(1 + percentMonthlyRate, countMonths) - 1)));
-  let totalSum = parseFloat((monthlyPayment * countMonths).toFixed(2));
   
-  return totalSum;
+  return parseFloat((monthlyPayment * countMonths).toFixed(2));
 }

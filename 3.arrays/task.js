7524cd583bdf9fe842befac2982af15c497f3ea0
Maksim Gavr <gavrilenk0.maksimik@ya.ru>
2024-07-08 15:04:24 +0700

    Minor revisions to the third task

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,3 +1,4 @@
+// Задача 1 (Сравнить массивы):
 function compareArrays(arr1, arr2) {
   return (arr1.length === arr2.length) && arr1.every((item, idx) => item === arr2[idx]);
 }
@@ -9,15 +10,17 @@ console.log(compareArrays([0, 1, 2], [0, 1]));
 console.log(compareArrays([0, 1], [0, 1, 2]));
 console.log(compareArrays([8, 9, 5, 4], [8, 9, 5, 4, 8, 3, 5]));
 
+// Задача 2 (Фильтрация и преобразование массива):
 function getUsersNamesInAgeRange(users, gender) {
   let result = users.filter(user => user.gender === gender).
   map(user => user.age).
   reduce((acc, item, idx, arr) => {
-  acc += item;
-  if (idx === arr.length - 1) {
-    return acc / arr.length;
-  }
-  return acc;
+    acc += item;
+    if (idx === arr.length - 1) {
+      return acc / arr.length;
+    }
+    
+    return acc;
   }, 0);  
     
   return result;
@@ -38,7 +41,7 @@ const people = [
   {firstName: "Владислав", secondName: "Давыдов", age: 40, gender: "мужской"},
   {firstName: "Илья", secondName: "Казаков", age: 35, gender: "мужской"},
   {firstName: "Евгений", secondName: "Кузьмин", age: 19, gender: "мужской"},
-]
+];
 
 console.log(getUsersNamesInAgeRange(people, "мужской"));
 console.log(getUsersNamesInAgeRange(people, "женский"));
